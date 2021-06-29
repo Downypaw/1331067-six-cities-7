0920950e4275155b9cd6e4d77f9863e592cd4106
Таня <tanamalceva274@gmail.com>
2021-06-29 13:50:39 +0300

    Обновлён компонент SignIn

--- project/src/components/sign-in/sign-in.jsx
+++ project/src/components/sign-in/sign-in.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import Logo from '../logo/logo';
+import {useHistory} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {login} from '../../store/api-actions';
+import {AppRoute} from '../../const';
 
-export default function SignIn() {
+export function SignIn({onSubmit}) {
+  const loginRef = useRef();
+  const passwordRef = useRef();
+
+  const history = useHistory();
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    onSubmit({
+      login: loginRef.current.value,
+      password: passwordRef.current.value,
+    });
+  };
+
   return (
     <div className="page page--gray page--login">
       <header className="header">
@@ -27,16 +46,36 @@ export default function SignIn() {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
+            <form className="login__form form" action="#" onSubmit={handleSubmit} method="post">
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
-                <input className="login__input form__input" type="email" name="email" placeholder="Email" required=""/>
+                <input
+                  ref={loginRef}
+                  className="login__input form__input"
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  required=""
+                />
               </div>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required=""/>
+                <input
+                  ref={passwordRef}
+                  className="login__input form__input"
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  required=""
+                />
               </div>
-              <button className="login__submit form__submit button" type="submit">Sign in</button>
+              <button
+                onClick={() => history.push(AppRoute.INDEX)}
+                className="login__submit form__submit button"
+                type="submit"
+              >
+                Sign in
+              </button>
             </form>
           </section>
           <section className="locations locations--login locations--current">
@@ -51,3 +90,15 @@ export default function SignIn() {
     </div>
   );
 }
+
+SignIn.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit(signInData) {
+    dispatch(login(signInData));
+  },
+});
+
+export default connect(null, mapDispatchToProps)(SignIn);
