48327054dcb5e84b7197cd147f5e83181b4bf9e4
Таня <tanamalceva274@gmail.com>
2021-07-09 18:47:38 +0300

    Осуществит создание actions с помощью RTK

--- project/src/store/action.js
+++ project/src/store/action.js
@@ -1,3 +1,5 @@
+import {createAction} from '@reduxjs/toolkit';
+
 export const ActionType = {
   CHANGE_CITY: '/cityChange',
   LOAD_OFFERS: 'data/loadOffers',
@@ -9,45 +11,34 @@ export const ActionType = {
   UPDATE_REVIEWS: 'comments/updateReviews',
 };
 
-export const changeCity = (city) => ({
-  type: ActionType.CHANGE_CITY,
+export const changeCity = createAction(ActionType.CHANGE_CITY, (city) => ({
   payload: city,
-});
+}));
 
-export const getOffers = () => ({
-  type: ActionType.FILTER_OFFERS,
-});
+export const getOffers = createAction(ActionType.FILTER_OFFERS);
 
-export const loadOffers = (offers) => ({
-  type: ActionType.LOAD_OFFERS,
+export const loadOffers = createAction(ActionType.LOAD_OFFERS, (offers) => ({
   payload: offers,
-});
+}));
 
-export const requireAuthorization = (status) => ({
-  type: ActionType.REQUIRED_AUTHORIZATION,
+export const requireAuthorization = createAction(ActionType.REQUIRED_AUTHORIZATION, (status) => ({
   payload: status,
-});
+}));
 
-export const makeLogout = () => ({
-  type: ActionType.LOGOUT,
-});
+export const makeLogout = createAction(ActionType.LOGOUT);
 
-export const redirectToRoute = (url) => ({
-  type: ActionType.REDIRECT_TO_ROUTE,
+export const redirectToRoute = createAction(ActionType.REDIRECT_TO_ROUTE, (url) => ({
   payload: url,
-});
+}));
 
-export const updateOffer = (offer) => ({
-  type: ActionType.UPDATE_OFFER,
+export const updateOffer = createAction(ActionType.UPDATE_OFFER, (offer) => ({
   payload: offer,
-});
+}));
 
-export const loadFullOfferInformation = (detailedOfferData, nearbyOffersData, reviewsData) => ({
-  type: ActionType.LOAD_FULL_OFFER_INFORMATION,
+export const loadFullOfferInformation = createAction(ActionType.LOAD_FULL_OFFER_INFORMATION, (detailedOfferData, nearbyOffersData, reviewsData) => ({
   payload: {detailedOfferData, nearbyOffersData, reviewsData},
-});
+}));
 
-export const updateReviews = (reviews) => ({
-  type: ActionType.UPDATE_REVIEWS,
+export const updateReviews = createAction(ActionType.UPDATE_REVIEWS, (reviews) => ({
   payload: reviews,
-});
+}));
