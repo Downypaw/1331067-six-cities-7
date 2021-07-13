50563347ba4b28fd7fb10ce196fc04f908b804d5
Таня <tanamalceva274@gmail.com>
2021-07-13 23:45:51 +0300

    Добавит тесты для асинхронных операций

---
+++ project/src/store/api-actions-mock.js
@@ -0,0 +1,34 @@
+export const updatedOfferFromServer = {
+  'bedrooms': 3,
+  'city': {
+    'location': {
+      'latitude': 52.38333,
+      'longitude': 4.9,
+      'zoom': 10,
+    },
+    'name': 'Amsterdam',
+  },
+  'description': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+  'goods': ['Heating', 'Kitchen', 'Cable TV', 'Washing machine', 'Coffee machine', 'Dishwasher'],
+  'host': {
+    'avatar_url': 'img/avatar-angelina.jpg',
+    'id': 1,
+    'is_pro': true,
+    'name': 'Angelina',
+  },
+  'id': 1,
+  'images': ['img/room.jpg', 'img/apartment-01.jpg', 'img/apartment-02.jpg', 'img/apartment-03.jpg', 'img/apartment-01.jpg', 'img/apartment-02.jpg'],
+  'is_favorite': true,
+  'is_premium': false,
+  'location': {
+    'latitude': 52.3909553943508,
+    'longitude': 4.85309666406198,
+    'zoom': 8,
+  },
+  'max_adults': 4,
+  'preview_image': 'img/room.jpg',
+  'price': 120,
+  'rating': 4.8,
+  'title': 'Beautiful & luxurious studio at great location',
+  'type': 'apartment',
+}
---
+++ project/src/store/api-actions.test.js
@@ -0,0 +1,223 @@
+import MockAdapter from 'axios-mock-adapter';
+import {createAPI} from '../services/api';
+import {ActionType} from './action';
+import {updatedOfferFromServer} from './api-actions-mock';
+import {checkAuth, login, fetchOffers, logout, toggleFavorite, getFullOfferInformation, postComment, getFavoriteOffers} from './api-actions';
+import {APIRoute, AppRoute, AuthorizationStatus} from '../const';
+import {adaptOfferToClient} from '../util/adapter';
+
+let api = null;
+
+describe('Async operations', () => {
+  beforeAll(() => {
+    api = createAPI(() => {});
+  });
+
+  it('should make a correct API call to GET /login', () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const checkAuthLoader = checkAuth();
+
+    apiMock
+      .onGet(APIRoute.LOGIN)
+      .reply(200, [{fake: true}]);
+
+    return checkAuthLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.REQUIRED_AUTHORIZATION,
+          payload: AuthorizationStatus.AUTH,
+        });
+      });
+  });
+
+  it('should make a correct API call to POST /login', () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const fakeUser = {email: 'test@test.ru', password: '123456'};
+    const loginLoader = login(fakeUser);
+
+    apiMock
+      .onPost(APIRoute.LOGIN)
+      .reply(200, [{fake: true}]);
+
+    return loginLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(2);
+
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.REQUIRED_AUTHORIZATION,
+          payload: AuthorizationStatus.AUTH,
+        });
+
+        expect(dispatch).toHaveBeenNthCalledWith(2, {
+          type: ActionType.REDIRECT_TO_ROUTE,
+          payload: AppRoute.INDEX,
+        });
+      });
+  });
+
+  it('should make a correct API call to GET /offers', () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const offersLoader = fetchOffers();
+
+    apiMock
+      .onGet(APIRoute.OFFERS)
+      .reply(200, []);
+
+    return offersLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.LOAD_OFFERS,
+          payload: [],
+        });
+      });
+  });
+
+  it('should make a correct API call to DELETE /logout', () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const logoutLoader = logout();
+
+    apiMock
+      .onDelete(APIRoute.LOGOUT)
+      .reply(200);
+
+    return logoutLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.LOGOUT,
+        });
+      });
+  });
+
+  it('should make a correct API call to POST /favorite/:offerId/:status', () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const offerId = 1;
+    const status = 0;
+    const toggleFavoriteLoader = toggleFavorite({offerId, status});
+
+    apiMock
+      .onPost(`${APIRoute.FAVORITE}/${offerId}/${status}`)
+      .reply(200, updatedOfferFromServer);
+
+    return toggleFavoriteLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.UPDATE_OFFER,
+          payload: adaptOfferToClient(updatedOfferFromServer),
+        });
+      });
+  });
+
+  it('should make a correct API call to GET full offer information', () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const offerId = 1;
+    const toggleFavoriteLoader = getFullOfferInformation(offerId);
+
+    apiMock
+      .onGet(`${APIRoute.OFFERS}/${offerId}`)
+      .reply(200, updatedOfferFromServer);
+
+    apiMock
+      .onGet(`${APIRoute.OFFERS}/${offerId}/nearby`)
+      .reply(200, []);
+
+    apiMock
+      .onGet(`${APIRoute.REVIEWS}/${offerId}`)
+      .reply(200, []);
+
+    return toggleFavoriteLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.LOAD_FULL_OFFER_INFORMATION,
+          payload: {
+            detailedOfferData: adaptOfferToClient(updatedOfferFromServer),
+            nearbyOffersData: [],
+            reviewsData: [],
+          },
+        });
+      });
+  });
+
+  it('should make a correct API call to GET full offer information', () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const offerId = 1;
+    const toggleFavoriteLoader = getFullOfferInformation(offerId);
+
+    apiMock
+      .onGet(`${APIRoute.OFFERS}/${offerId}`)
+      .reply(200, updatedOfferFromServer);
+
+    apiMock
+      .onGet(`${APIRoute.OFFERS}/${offerId}/nearby`)
+      .reply(200, []);
+
+    apiMock
+      .onGet(`${APIRoute.REVIEWS}/${offerId}`)
+      .reply(200, []);
+
+    return toggleFavoriteLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.LOAD_FULL_OFFER_INFORMATION,
+          payload: {
+            detailedOfferData: adaptOfferToClient(updatedOfferFromServer),
+            nearbyOffersData: [],
+            reviewsData: [],
+          },
+        });
+      });
+  });
+
+  it('should make a correct API call to POST comment', () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const offerId = 1;
+    const comment = '';
+    const rating = 1;
+    const commentLoader = postComment(offerId, {comment, rating});
+
+    apiMock
+      .onPost(`${APIRoute.REVIEWS}/${offerId}`)
+      .reply(200, []);
+
+    return commentLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.UPDATE_REVIEWS,
+          payload: [],
+        });
+      });
+  });
+
+  it('should make a correct API call to POST comment', () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const favoriteOffersLoader = getFavoriteOffers();
+
+    apiMock
+      .onGet(APIRoute.FAVORITE)
+      .reply(200, []);
+
+    return favoriteOffersLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.LOAD_FAVORITE_OFFERS,
+          payload: [],
+        });
+      });
+  });
+});
