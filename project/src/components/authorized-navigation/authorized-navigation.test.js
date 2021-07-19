1f268a859368b439b2972d75b8b26c9f6b9bc67d
Таня <tanamalceva274@gmail.com>
2021-07-19 11:11:01 +0300

    Добавит тест для копонента AuthorizedNavigation

---
+++ project/src/components/authorized-navigation/authorized-navigation.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Router, Route} from 'react-router-dom';
+import {createMemoryHistory} from 'history';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import configureStore from 'redux-mock-store';
+import {Provider} from 'react-redux';
+import AuthorizedNavigation from './authorized-navigation';
+import {logout} from '../../store/api-actions';
+import {AppRoute} from '../../const.js';
+
+let history = null;
+let store = null;
+let fakeComponent = null;
+
+describe('Component: AuthorizedNavigation', () => {
+  beforeAll(() => {
+    window.localStorage.__proto__.getItem = jest.fn();
+
+    history = createMemoryHistory();
+
+    const createFakeStore = configureStore({});
+
+    store = createFakeStore({});
+
+    fakeComponent = (
+      <Provider store={store}>
+        <Router history={history}>
+          <AuthorizedNavigation />
+        </Router>
+      </Provider>
+    );
+  });
+
+  it('should render correctly', () => {
+    render(fakeComponent);
+
+    expect(screen.getByText(/Sign out/i)).toBeInTheDocument();
+  });
+
+  it('should render login', () => {
+    render(fakeComponent);
+
+    expect(localStorage.getItem).toHaveBeenCalledWith('login');
+  });
+});
