934df22f3367d5356d57f5a258bb6e5729c49cfe
Таня <tanamalceva274@gmail.com>
2021-07-22 21:46:47 +0300

    Добавит тест для компонента UnauthorizedNavigation

--- project/src/components/unauthorized-navigation/unauthorized-navigation.test.js
+++ project/src/components/unauthorized-navigation/unauthorized-navigation.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import {render} from '@testing-library/react';
-import {Router} from 'react-router-dom';
+import {render, screen} from '@testing-library/react';
+import {Router, Route} from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import {createMemoryHistory} from 'history';
 import UnauthorizedNavigation from './unauthorized-navigation';
+import {AppRoute} from '../../const';
 
 describe('Component: UnauthorizedNavigation', () => {
   it('should render correctly', () => {
@@ -15,5 +17,19 @@ describe('Component: UnauthorizedNavigation', () => {
     const linkElement = getByText('Sign in');
 
     expect(linkElement).toBeInTheDocument();
+  });
+
+  it('should render correctly', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={history}>
+        <UnauthorizedNavigation />
+        <Route exact path={AppRoute.SIGN_IN}>Mock Sign In Screen</Route>
+      </Router>,
+    );
+
+    userEvent.click(screen.getByText('Sign in'));
+
+    expect(screen.getByText('Mock Sign In Screen')).toBeInTheDocument();
   });
 });
